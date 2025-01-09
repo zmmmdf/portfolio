70437cf3d30ee4dfd9a984a83ceaed4eab40b3cd
Ziya <ziyamm08@gmail.com>
2025-01-09 21:08:59 +0400

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -164,9 +164,9 @@ export default function Page() {
                   I like building things
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  During my time in university, I attended{" "}
+                  During my time in school, I attended{" "}
                   {DATA.hackathons.length}+ hackathons. People from around the
-                  country would come together and build incredible things in 2-3
+                  country would come together and build incredible things in 1-2
                   days. It was eye-opening to see the endless possibilities
                   brought to life by a group of motivated and passionate
                   individuals.
@@ -211,10 +211,9 @@ export default function Page() {
                   href={DATA.contact.social.X.url}
                   className="text-blue-500 hover:underline"
                 >
-                  with a direct question on twitter
+                  with a direct question on X
                 </Link>{" "}
-                and I&apos;ll respond whenever I can. I will ignore all
-                soliciting.
+                and I&apos;ll respond whenever I can.
               </p>
             </div>
           </BlurFade>
